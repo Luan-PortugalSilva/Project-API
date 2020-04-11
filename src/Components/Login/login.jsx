618e82d53d2c5ba3a94c43de1213308b50e0814f
Luan Portugal <luanportugal.contato@gmail.com>
2020-04-11 14:14:45 -0300

    Stylizing the login screen

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import '../Login/login.css'
 
 export default Login => {
     return (
-        <div className="background">
-            <form>
-                <input className="login" placeholder="User"></input>
-                <input className="password" placeholder="password"></input>
-                <button type="submit">Login</button>
-            </form>
+        <div className="container">
+            <h1>LOGIN</h1>
+            <input className="user" placeholder="User"></input>
+            <input className="password" placeholder="password"></input>
+            <button type="submit" className="buttom-login">LOGIN</button>
+
+            <div className="container-flex">
+                <p><a href="/register" className="register-login">Register</a></p>
+                <p><a href="/forgot" className="forgot-login">Forgot</a></p>
+            </div>
+
         </div>
     )
 }
--- src/Components/Register/register.jsx
+++ src/Components/Register/register.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import './register.css' 
+import './register.css'
 
 export default Register => {
     return (
         <div className="background">
             <h1>SINGUP USER</h1>
-                <input className="login" placeholder="User"></input>
-                <input className="email" placeholder="E-mail"></input>
-                <input className="password" placeholder="Password"></input>
-                <input className="password" placeholder="Confirm your password"></input>
-                <button type="submit">REGISTER</button>
-<p>U have account? <a href='#'> LOGIN</a> </p>
-                    
-                </div>
+            <input className="login" placeholder="User"></input>
+            <input className="email" placeholder="E-mail"></input>
+            <input className="password" placeholder="Password"></input>
+            <input className="password" placeholder="Confirm your password"></input>
+            <button type="submit">REGISTER</button>
+
+            <div className="container-flex">
+                <p><a href="/" className="register-register">Login</a></p>
+                <p><a href="/forgot" className="forgot-register">Forgot</a></p>
+            </div>
+
+        </div>
     )
 }
