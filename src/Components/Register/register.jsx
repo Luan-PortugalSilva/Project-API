845a2da0176581484c26518b4ec24124da5984d7
Pedro Lucas <pedrolucasfsouza@gmail.com>
2020-04-11 13:36:58 -0300

    Register page frontend

--- src/Components/Register/register.jsx
+++ src/Components/Register/register.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import './register.css' 
 
 export default Register => {
     return (
         <div className="background">
-            <form>
+            <h1>SINGUP USER</h1>
                 <input className="login" placeholder="User"></input>
-                <input className="password" placeholder="password"></input>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+                <input className="email" placeholder="E-mail"></input>
+                <input className="password" placeholder="Password"></input>
+                <input className="password" placeholder="Confirm your password"></input>
+                <button type="submit">REGISTER</button>
+<p>U have account? <a href='#'> LOGIN</a> </p>
+                    
+                </div>
     )
 }
